c1df3dafab60c9887cabaf9fce1c4bd14878f697
İmranov <semtexpert@gmail.com>
2023-12-27 16:43:23 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1 ile 100 arasındaki asal sayılar:");
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal = false;
+                    break;
+                }
+            }
+
+
+            if (asal) {
+                System.out.print(i + " ");
+            }
+        }
+
     }
 }
